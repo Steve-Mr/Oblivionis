61233941cd0e9ab308d1eb75f7a435d232a1d9ed
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-09-17 17:08:17 +0800

    init app screens basic layout

---
+++ app/src/main/java/com/example/ydaynomore/ui/ActionComponents.kt
@@ -0,0 +1,201 @@
+package com.example.ydaynomore.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import com.example.ydaynomore.R
+import kotlin.math.absoluteValue
+
+enum class YNMScreen() {
+    Recycle
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ActionScreen() {
+
+
+        val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text(
+                            "Centered Top App Bar",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior,
+                )
+            },
+
+            bottomBar = { ActionRow() }
+        ) { innerPadding ->
+
+//            NavHost()
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                val pagerState = rememberPagerState(pageCount = {
+                    10
+                })
+                HorizontalPager(state = pagerState,
+                    modifier = Modifier.padding(top = 16.dp),
+                    contentPadding = PaddingValues(horizontal = 64.dp),
+                    pageSpacing = 16.dp
+                ) { photo ->
+                    Card(
+                        Modifier
+                            .graphicsLayer {
+                                // Calculate the absolute offset for the current page from the
+                                // scroll position. We use the absolute value which allows us to mirror
+                                // any effects for both directions
+                                val pageOffset = (
+                                        (pagerState.currentPage - photo) +
+                                                pagerState
+                                            .currentPageOffsetFraction
+                                        ).absoluteValue
+
+                                // We animate the alpha, between 50% and 100%
+                                alpha = lerp(
+                                    start = 0.5f,
+                                    stop = 1f,
+                                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                )
+                            }
+                    ) {
+                        // Card content
+                        if (photo % 3 == 1) {
+                            Image(
+                                modifier = Modifier.clip(RoundedCornerShape(16.dp)),
+                                alignment = Alignment.Center,
+                                painter = painterResource(id = R.drawable.test),
+                                contentDescription = "")
+                        } else if (photo % 3 == 2) {
+                            Image(
+                                modifier = Modifier.clip(RoundedCornerShape(16.dp)),
+                                alignment = Alignment.Center,
+                                painter = painterResource(id = R.drawable.test1),
+                                contentDescription = "")
+                        } else {
+                            Image(
+                                modifier = Modifier.clip(RoundedCornerShape(16.dp)),
+                                alignment = Alignment.Center,
+                                painter = painterResource(id = R.drawable.test2),
+                                contentDescription = "")
+                        }
+                    }
+
+                    }
+                }
+            }
+        }
+
+@Composable
+fun ActionImage(modifier: Modifier, id: Int) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ){
+        Image(
+            modifier = Modifier.clip(RoundedCornerShape(16.dp)),
+            alignment = Alignment.Center,
+            painter = painterResource(id = id),
+            contentDescription = "")
+    }
+}
+
+@Composable
+fun ActionRow() {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween){
+        Box(modifier = Modifier.weight(2f),
+            contentAlignment = Alignment.CenterStart) {
+            Button(onClick = { /*TODO*/ }) {
+                Text("RB")
+            }
+        }
+
+        Box(modifier = Modifier.weight(3f)) {
+            Button(onClick = { /*TODO*/ }) {
+                Text("DEL")
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
+@Composable
+fun PreviewActionRow() {
+//    ActionRow()
+    ActionScreen()
+}
---
+++ app/src/main/java/com/example/ydaynomore/ui/RecycleCompoments.kt
@@ -0,0 +1,75 @@
+package com.example.ydaynomore.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ydaynomore.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecycleScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Recycle")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                
+            }
+        }
+    ) { innerPadding ->
+        RecycleContent(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun RecycleContent(modifier: Modifier) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        verticalItemSpacing = 4.dp,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        content = {
+            items(10) { photo ->
+                if (photo % 2 == 1) {
+                    ActionImage(modifier = Modifier
+                        .fillMaxWidth(fraction = 0.5f), R.drawable.test1)
+                } else {
+                    ActionImage(modifier = Modifier
+                        .fillMaxWidth(fraction = 0.5f), R.drawable.test2)
+                }
+            }
+        },
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RecycleScreenPreview() {
+    RecycleScreen()
+}
