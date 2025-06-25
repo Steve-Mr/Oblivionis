7371fc32e09d520cd4cbe252f3216d667d4000d0
Maary <24504742+Steve-Mr@users.noreply.github.com>
2025-06-25 22:00:37 +0800

    apply RoundedCornerShape to all screens

--- app/src/main/java/top/maary/oblivionis/OblivionisScreen.kt
+++ app/src/main/java/top/maary/oblivionis/OblivionisScreen.kt
@@ -5,13 +5,18 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -24,8 +29,8 @@ import top.maary.oblivionis.data.PreferenceRepository
 import top.maary.oblivionis.ui.screen.ActionScreen
 import top.maary.oblivionis.ui.screen.EntryScreen
 import top.maary.oblivionis.ui.screen.RecycleScreen
-import top.maary.oblivionis.ui.screen.WelcomeScreen
 import top.maary.oblivionis.ui.screen.SettingsScreen
+import top.maary.oblivionis.ui.screen.WelcomeScreen
 import top.maary.oblivionis.viewmodel.ActionViewModel
 import top.maary.oblivionis.viewmodel.NotificationViewModel
 
@@ -74,53 +79,62 @@ fun OblivionisApp(
         },
         navController = navController,
         startDestination =
-        if (runBlocking { dataStore.permissionGranted.first() }) {
-            Log.v("OBLIVIONIS", "GO TO ENTRY")
+            if (runBlocking { dataStore.permissionGranted.first() }) {
+                Log.v("OBLIVIONIS", "GO TO ENTRY")
 
-            OblivionisScreen.Entry.name
-        }
-                else OblivionisScreen.Welcome.name,
+                OblivionisScreen.Entry.name
+            }
+            else OblivionisScreen.Welcome.name,
     ){
         composable (route = OblivionisScreen.Welcome.name) {
             Log.v("OBLIVIONIS", "PERMISSION 2")
-
-            WelcomeScreen (onPermissionFinished = {
-                if (isReWelcome.value) return@WelcomeScreen
-                welcomePermissionBasicLogic()
-            } , onFabClicked = {
-                welcomePermissionBasicLogic()
-            })
+            Surface(modifier = Modifier.clip(RoundedCornerShape(16.dp))) {
+                WelcomeScreen (onPermissionFinished = {
+                    if (isReWelcome.value) return@WelcomeScreen
+                    welcomePermissionBasicLogic()
+                } , onFabClicked = {
+                    welcomePermissionBasicLogic()
+                })
+            }
         }
         composable (route = OblivionisScreen.Entry.name) {
-            EntryScreen(
-                viewModel = actionViewModel,
-                onAlbumClick = {
-                    actionViewModel.loadImages()
-                    navController.navigate(OblivionisScreen.Action.name) },
-                onSettingsClick = { navController.navigate(OblivionisScreen.Settings.name) }
-            )
+            Surface(modifier = Modifier.clip(RoundedCornerShape(16.dp))) {
+                EntryScreen(
+                    viewModel = actionViewModel,
+                    onAlbumClick = {
+                        actionViewModel.loadImages()
+                        navController.navigate(OblivionisScreen.Action.name) },
+                    onSettingsClick = { navController.navigate(OblivionisScreen.Settings.name) }
+                )
+            }
         }
         composable (route = OblivionisScreen.Action.name) {
-            ActionScreen (
-                viewModel = actionViewModel,
-                onNextButtonClicked = { navController.navigate(OblivionisScreen.Recycle.name)},
-                onBackButtonClicked = { navController.popBackStack() })
+            Surface(modifier = Modifier.clip(RoundedCornerShape(16.dp))) {
+                ActionScreen (
+                    viewModel = actionViewModel,
+                    onNextButtonClicked = { navController.navigate(OblivionisScreen.Recycle.name)},
+                    onBackButtonClicked = { navController.popBackStack() })
+            }
         }
         composable (route = OblivionisScreen.Recycle.name) {
-            RecycleScreen(
-                actionViewModel = actionViewModel,
-                notificationViewModel = notificationViewModel,
-                onBackButtonClicked = {navController.popBackStack()})
+            Surface(modifier = Modifier.clip(RoundedCornerShape(16.dp))) {
+                RecycleScreen(
+                    actionViewModel = actionViewModel,
+                    notificationViewModel = notificationViewModel,
+                    onBackButtonClicked = {navController.popBackStack()})
+            }
         }
         composable (route = OblivionisScreen.Settings.name) {
-            SettingsScreen(
-                onReWelcomeClick = {
-                    scope.launch { dataStore.setReWelcome(true) }
-                    welcomeScreenNextDest.value = OblivionisScreen.Settings.name
-                    navController.navigate(OblivionisScreen.Welcome.name) },
-                onBackButtonClicked = { navController.popBackStack() },
-                notificationViewModel = notificationViewModel
-            )
+            Surface(modifier = Modifier.clip(RoundedCornerShape(16.dp))) {
+                SettingsScreen(
+                    onReWelcomeClick = {
+                        scope.launch { dataStore.setReWelcome(true) }
+                        welcomeScreenNextDest.value = OblivionisScreen.Settings.name
+                        navController.navigate(OblivionisScreen.Welcome.name) },
+                    onBackButtonClicked = { navController.popBackStack() },
+                    notificationViewModel = notificationViewModel
+                )
+            }
         }
     }
 }
